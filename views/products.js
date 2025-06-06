c113cebf6754870d6bea1a48de49872ca1017c84
Stuart Winston <stuartwinsto@student.unimelb.edu.au>
2025-06-06 19:21:24 +1000

    Added add-to-cart feature where item is now updated in the cart.html page using queries in products.js

--- views/products.js
+++ views/products.js
@@ -1,13 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".accordion-item h3").forEach((header) => {
-    header.addEventListener("click", () => {
-      const content = header.nextElementSibling;
-      content.style.display =
-        content.style.display === "block" ? "none" : "block";
-    });
-  });
+// document.addEventListener("DOMContentLoaded", () => {
+// document.querySelectorAll(".accordion-item h3").forEach((header) => {
+// header.addEventListener("click", () => {
+// const content = header.nextElementSibling;
+// content.style.display =
+// content.style.display === "block" ? "none" : "block";
+// });
+//  });
 
-  document.querySelector(".add-to-cart").addEventListener("click", () => {
-    alert("Added to cart!");
-  });
-});
+const PRODUCTS = {
+  MK514: {
+    id: "MK514",
+    name: "EBike100 Electric Cargo Bike MK514/MK516 + NFC Card",
+    price: 850,
+    description: "Battery Capacity 48V 30AH / Wheel Size (inch) 14",
+    image: "../assets/images/MK514.png",
+  },
+  MK220: {
+    id: "MK220",
+    name: "Electric Cargo Bike MK220",
+    price: 950,
+    description: "Dual Seat / 48V 30AH / 14-Inch Tires",
+    image: "../assets/images/MK220.png",
+  },
+};
+
+function addToCart(product) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.push(product);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Item added to cart!"); //Replace with animation//
+}
